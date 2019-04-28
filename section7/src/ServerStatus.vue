bf3a10db46fc0ab2898abaf5f90249b50f19a89b
Tyler Merry <tmerry@fool.com>
2019-04-28 10:59:18 -0600

    finishing chap 7

---
+++ section7/src/ServerStatus.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>Server Status: {{status}}</p>
+    <button @click="changeStatus">Reset Status</button>
+  </div>
+</template>
+
+<script>
+ export default{
+     data: function(){
+         return {
+             status: "Critical"
+         }
+     },
+     methods:{
+         changeStatus() {
+             this.status = 'Normal'
+         }
+     }
+ }
+</script>
+
+<style scoped>
+ div{
+     border: 1px solid blue
+ }
+</style>
