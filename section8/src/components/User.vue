dd19a8474faf301b7a57b0a1b346009ec4216713
Tyler Merry <tmerry@fool.com>
2019-04-28 11:25:28 -0600

    showing sending data to and from a component

--- section8/src/components/User.vue
+++ section8/src/components/User.vue
@@ -1,29 +1,41 @@
 <template>
-    <div class="component">
-        <h1>The User Component</h1>
-        <p>I'm an awesome User!</p>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
-            </div>
-        </div>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User!....{{name}}</p>
+    <button @click="changeName">Change my name</button>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <!-- this will send data to the component, and allow you to listen to events-->
+        <app-user-detail :name="name" @nameWasReset="name = $event"></app-user-detail>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <app-user-edit></app-user-edit>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import UserDetail from './UserDetail.vue';
-    import UserEdit from './UserEdit.vue';
+ import UserDetail from './UserDetail.vue';
+ import UserEdit from './UserEdit.vue';
 
-    export default {
-        components: {
-            appUserDetail: UserDetail,
-            appUserEdit: UserEdit
-        }
-    }
+ export default {
+     data: function(){
+         return {
+          name: "Tyler"
+         }
+     },
+     methods:{
+         changeName(){
+             this.name = "Randi";
+         }
+     },
+     components: {
+         appUserDetail: UserDetail,
+         appUserEdit: UserEdit
+     }
+ }
 </script>
 
 <style scoped>
--- section8/src/components/UserDetail.vue
+++ section8/src/components/UserDetail.vue
@@ -1,15 +1,35 @@
 <template>
-    <div class="component">
-        <h3>You may view the User Details here</h3>
-        <p>Many Details</p>
-    </div>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Name: {{ name }}</p>
+    <p>Reversed Name: {{ reverseName() }}</p>
+    <button @click="resetName">Reset Name</button>
+
+  </div>
 </template>
 
 <script>
+ export default{
+     props:{
+         "name":{
+             type: String,
+             required: true,
+         },
+     },
+     methods: {
+         reverseName(){
+             return this.name.split("").reverse().join("");
+         },
+         resetName(){
+             this.name = 'tyler';
+             this.$emit('nameWasReset', this.name);
+         }
+     }
+ }
 </script>
 
 <style scoped>
-    div {
-        background-color: lightcoral;
-    }
+ div {
+     background-color: lightcoral;
+ }
 </style>
