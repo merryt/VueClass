a23af5ac5903894fd3e0eabe54b99c2ef6adcefe
Tyler Merry <tmerry@fool.com>
2019-06-23 10:08:23 -0600

    Chapter 11

---
+++ section11/src/Switch.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div id="on"
+         @click="switched(true)"
+         :class="{active: value}"
+    >On</div>
+
+    <div id="off"
+         @click="switched(false)"
+         :class="{active: !value}"
+    >Off</div>
+
+  </div>
+</template>
+
+
+<script>
+ /*
+
+    To use components as controls with v-model they need to have a prop with the name of value,
+    and a emit with input.
+  */
+ export default {
+     props: ['value'],
+     methods: {
+         switched(updatedValue){
+             this.value=updatedValue
+             this.$emit('input', updatedValue)
+         }
+     }
+ }
+</script>
+
+
+<style scoped>
+ #on, #off{
+     width: 40px;
+     height: 20px;
+     background-color: lightgray;
+     padding: 2px;
+     display: inline-block;
+     margin: 10px -2px;
+     box-sizing: content-box;
+     cursor: poiinter;
+     text-align: center;
+ }
+
+ #on:hover, #on.active{
+     background-color: lightgreen;
+
+ }
+ #off:hover, #off.active{
+     background-color: lightcoral;
+
+ }
+</style>
